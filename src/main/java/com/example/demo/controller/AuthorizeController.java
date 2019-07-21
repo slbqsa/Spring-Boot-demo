be47e884baa76f46324f169ff31c21d3a3defba0
13891362942@wo.cn <13891362942@wo.cn>
2019-07-21 09:08:31 +0800

    提交

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
-
 import com.example.demo.dto.AccessTokenDTO;
-import com.example.demo.provider.Githubprovider;
+import com.example.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +12,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AuthorizeController {
     @Autowired
-    private Githubprovider githubprovider;
-
+    private GithubProvider githubProvider;
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
-        accessTokenDTO.setState(state);
-        accessTokenDTO.setClient_id(" Iv1.8aa52cefb3151283");
         accessTokenDTO.setClient_secret("64f4674e1c0bf8d5da9bb59be1b393dc418f7ff5");
-        githubprovider.getAccessToken(accessTokenDTO);
+        accessTokenDTO.setClient_id("Iv1.8aa52cefb3151283");
+        githubProvider.getAccessToken(accessTokenDTO);
         return "index";
     }
 }
--- src/main/java/com/example/demo/dto/GithubUser.java
+++ src/main/java/com/example/demo/dto/GithubUser.java
@@ -2,7 +2,7 @@ package com.example.demo.dto;
 
 public class GithubUser {
     private String name;
-    private  Long id;
+    private Long id;
     private String bio;
 
     public String getName() {
