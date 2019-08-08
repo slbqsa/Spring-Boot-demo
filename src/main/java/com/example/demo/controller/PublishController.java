d4b1a26e8bdc8459474dd9f57e0af891e29d258a
13891362942@wo.cn <13891362942@wo.cn>
2019-08-08 14:40:39 +0800

    修改

--- src/main/java/com/example/demo/controller/PublishController.java
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -33,6 +33,18 @@ public class PublishController {
             @RequestParam("tag") String tag,
             HttpServletRequest request,
             Model model){
+        if(title== null && title==""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if(description== null && description==""){
+            model.addAttribute("error","问题补充不能为空");
+            return "publish";
+        }
+        if(tag== null && tag==""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
 
         model.addAttribute("title",title);
         model.addAttribute("description",description);
